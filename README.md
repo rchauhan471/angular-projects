ca2cd798d730fd71d08680f94ded38b39a763ffa
rchauhan471 <rchauhan471@rku.ac.in>
2021-12-09 13:58:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wb5hkl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wb5hkl)
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../model/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  inputTodo = '';
+
+  constructor() {}
+
+  ngOnInit() {
+    this.todos = [];
+  }
+  toggleDone(id) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    });
+  }
+
+  deleteTodo(id) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+    this.inputTodo = '';
+  }
+}
